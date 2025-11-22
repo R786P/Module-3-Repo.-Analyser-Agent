f44527d57580178e894931797337f49bc6366f75
Rahul Bunker <rahulbunker6255@gmail.com>
2025-11-22 23:05:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, request, jsonify
-from src.agents import run_analysis
+from agents import run_analysis
 
 # Global app object — Render ke liye zaroori
 app = Flask(__name__)
